88c8765ddbf18972c7218ff7eaae0d1db8799714
Zhou-Yicheng <11810925@mail.sustech.edu.cn>
2023-08-17 16:47:39 +0800

    add tag 0

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -169,7 +169,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9", '0' }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
@@ -227,8 +227,21 @@ root.buttons(gears.table.join(
 ))
 -- }}}
 
+function gettags()
+    local screen = awful.screen.focused()
+    return screen.tags
+end
+
+function viewnextactive()
+end
+
+function viewprevactive()
+end
+
 -- {{{ Key bindings
 globalkeys = gears.table.join(
+    awful.key({ modkey,           }, '`',      awful.tag.viewnext),
+    awful.key({ modkey, 'Shift'   }, '`',      awful.tag.viewprev),
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
@@ -381,7 +394,7 @@ clientkeys = gears.table.join(
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it work on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
-for i = 1, 9 do
+for i = 1, 10 do
     globalkeys = gears.table.join(globalkeys,
         -- View tag only.
         awful.key({ modkey }, "#" .. i + 9,
