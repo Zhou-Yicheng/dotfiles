0216c6bc34a5b8c428da2070d7202e754652b4dc
Zhou-Yicheng <zhou_yicheng@foxmail.com>
2023-10-15 16:03:57 +0800

    replace leap.nvim with flash.nvim

--- init.lua
+++ init.lua
@@ -1,53 +1,59 @@
 vim.opt.ignorecase = true
+vim.opt.clipboard = 'unnamedplus'
 
 local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
 
 if not vim.loop.fs_stat(pckr_path) then
-vim.fn.system({
-  'git',
-  'clone',
-  "--filter=blob:none",
-  'https://github.com/lewis6991/pckr.nvim',
-  pckr_path
-})
+  vim.fn.system({
+    'git',
+    'clone',
+    "--filter=blob:none",
+    'https://github.com/lewis6991/pckr.nvim',
+    pckr_path
+  })
 end
 
 vim.opt.rtp:prepend(pckr_path)
 
-require('pckr').add{
+local keys = require('pckr.loader.keys')
+require('pckr').add {
   {
-    'ggandor/leap.nvim',
-    requires = 'tpope/vim-repeat',
-    config = function() require('leap').add_default_mappings() end,
-  };
+    'folke/flash.nvim',
+    cond = {
+      keys({ 'n', 'x', 'o' }, 's', function() require('flash').jump() end, { desc = 'Flash' }),
+      keys({ 'n', 'x', 'o' }, 'S', function() require('flash').treesitter() end, { desc = 'Flash Treesitter' }),
+      keys({ 'o' }, 'r', function() require('flash').remote() end, { desc = 'Remote Flash' }),
+      keys({ 'o', 'x' }, 'R', function() require('flash').treesitter_search() end, { desc = 'Treesitter Search' }),
+      keys({ 'c' }, '<c-s>', function() require('flash').toggle() end, { desc = 'Toggle Flash Search' }),
+    },
+  },
   {
     'echasnovski/mini.nvim',
     config = function()
       require('mini.ai').setup()
-      -- require('mini.animate').setup()
       require('mini.comment').setup()
       require('mini.operators').setup()
       require('mini.pairs').setup()
       require('mini.splitjoin').setup() -- 'gS'
       require('mini.surround').setup({
         mappings = {
-          add = "gsa", -- Add surrounding in Normal and Visual modes
-          delete = "gsd", -- Delete surrounding
-          find = "gsf", -- Find surrounding (to the right)
-          find_left = "gsF", -- Find surrounding (to the left)
-          highlight = "gsh", -- Highlight surrounding
-          replace = "gsr", -- Replace surrounding
+          add = "gsa",            -- Add surrounding in Normal and Visual modes
+          delete = "gsd",         -- Delete surrounding
+          find = "gsf",           -- Find surrounding (to the right)
+          find_left = "gsF",      -- Find surrounding (to the left)
+          highlight = "gsh",      -- Highlight surrounding
+          replace = "gsr",        -- Replace surrounding
           update_n_lines = "gsn", -- Update `n_lines`
         }
       })
     end,
-  };
+  },
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
     requires = 'nvim-treesitter/nvim-treesitter',
     config = function()
       require("nvim-treesitter.configs").setup({
-        highlight = { enable = not(vim.g.vscode) },
+        highlight = { enable = not (vim.g.vscode) },
         indent = { enable = true },
         ensure_installed = {
           'astro', 'css', 'glimmer', 'graphql', 'html', 'javascript',
@@ -74,7 +80,7 @@ require('pckr').add{
         },
       })
     end
-  };
+  },
   {
     'JoosepAlviste/nvim-ts-context-commentstring',
     requires = 'nvim-treesitter/nvim-treesitter',
@@ -83,5 +89,5 @@ require('pckr').add{
         enable_autucmd = false,
       })
     end,
-  };
+  },
 }
